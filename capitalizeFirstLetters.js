3c332a9439518c773c80bc2ee5313a1a960338e2
SalemDrenay <salemdrenay@gmail.com>
2020-10-14 12:34:31 +0200

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -12,13 +12,8 @@ function capitalizeFirstLetters(input) {
     return output.join(' ');
 }
 
-// capitalizeFirstLetters est une fonction
 assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-// chaîne avec plusieurs mots
 assert.strictEqual(capitalizeFirstLetters('for fucking real'), 'For Fucking Real');
-// chaîne avec un seul mot
 assert.strictEqual(capitalizeFirstLetters('seriously'), 'Seriously');
-// chaîne avec caractère unique
 assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-// chaîne vide
 assert.strictEqual(capitalizeFirstLetters(''), '');
